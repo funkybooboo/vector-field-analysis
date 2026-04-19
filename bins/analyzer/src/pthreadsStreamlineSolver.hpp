2b65feae731b112b651eaaa21e354b54924d75d2
Nate Stott <nate.stott@pm.me>
2026-04-18 22:46:53 -0600

    fix issues

--- bins/analyzer/src/pthreadsStreamlineSolver.hpp
+++ bins/analyzer/src/pthreadsStreamlineSolver.hpp
@@ -3,7 +3,6 @@
 #include "streamlineSolver.hpp"
 
 #include <pthread.h>
-#include <vector>
 
 class PthreadsStreamlineSolver : public StreamlineSolver {
   public:
@@ -11,17 +10,6 @@ class PthreadsStreamlineSolver : public StreamlineSolver {
 
     void computeTimeStep(Field::Grid& grid) override;
 
-    struct ThreadArgs {
-        Field::Grid* grid = nullptr;
-        Field::GridCell* neighbors = nullptr;
-        int colCount = 0;
-        std::size_t startRow = 0;
-        std::size_t endRow = 0;
-    };
-
   private:
-    static void launchPass(std::vector<pthread_t>& threads, std::vector<ThreadArgs>& args,
-                           void* (*worker)(void*), const char* errorMsg);
-
     unsigned int threadCount_;
 };
