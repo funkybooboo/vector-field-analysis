f1104de28f3b71dff7b63ff573f955aec3161492
Rylei Mindrum <rymindrum@gmail.com>
2026-04-19 12:35:44 -0600

    comment for hybrid

--- bins/analyzer/src/solverFactory.hpp
+++ bins/analyzer/src/solverFactory.hpp
@@ -5,9 +5,9 @@
 #include <string_view>
 
 // Returns the StreamlineSolver implementation for the given name.
-// name must be one of: "sequential", "openmp", "pthreads", "mpi", "cuda".
+// name must be one of: "sequential", "openmp", "pthreads", "mpi", "cuda", "hybrid".
 // threadCount is forwarded to OpenMP/Pthreads/MPI (ignored by others).
-// cudaBlockSize is forwarded to CUDA solvers (ignored by others).
+// cudaBlockSize is forwarded to CUDA-based solvers (ignored by others).
 // Throws std::runtime_error for unknown names.
 [[nodiscard]] std::unique_ptr<StreamlineSolver>
 makeSolver(std::string_view name, unsigned int threadCount, unsigned int cudaBlockSize = 256);
